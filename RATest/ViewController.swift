29b6fc19791a9a3c3dcab213767b6e38d656145c
Paulo Antonio Nakama Kucher <paulokucher@hotmail.com>
2020-02-26 12:29:36 -0300

    Initial Commit

---
+++ RATest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RATest
+//
+//  Created by brq on 26/02/20.
+//  Copyright © 2020 brq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
